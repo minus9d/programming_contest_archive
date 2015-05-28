0cc310b1f26a2568a141330f6ab811871b54201c
minus9d <minus9d@gmail.com>
2015-05-28 22:37:57 +0900

    コメント、変数名など修正

--- gcj/gcj2013/round2/a/a.s.later.py
+++ gcj/gcj2013/round2/a/a.s.later.py
@@ -10,9 +10,9 @@ def calc_score(data, N):
     k = e - o + 1  # 通過駅数  同じ駅で降りたら1, 1駅移動したら2
     return ((k - 1) * (N - 1) - (k - 1) * k // 2) * p
 
-def calc_whole_score(o_e, N):
+def calc_whole_score(oep, N):
     score = 0
-    for tpl in o_e:
+    for tpl in oep:
         score += calc_score(tpl, N)
     return score
 
@@ -37,52 +37,51 @@ def pick_longest(stations, i):
     return (i,end+1,mn)
 
 
+# 入力の経路が以下の場合を考える
+#   (0)-(1)-(2)              * 2
+#           (2)-(3)-(4)-(5)  * 1
+# 駅と駅の間の通過数を取り出して足し合わせる
+#      2   2
+#              1   1   1
+#   ↓
+#      2   2   1   1   1
+# 22111から長い数字をgreedyにとっていく
+#   11111
+#   11
+# (2)で乗って(2)で降りる分が表せていないが、運賃ゼロなので問題ない
 def solve():
     N,M = map(int, input().split())
 
-    # 経路を数字におとす
-    #   (1)-(2)-(3)              * 2
-    #           (3)-(4)-(5)-(6)  
-    #    ↓
-    #      2   2
-    #              1   1   1
-    #   ↓
-    #      2   2   1   1   1
-    # 22111から長い数字をgreedyにとっていく
-    #   11111
-    #   11
-    # (3)で乗って(3)で降りる分は運賃ゼロなので無視してよい
-    
     stations = [0] * (N-1)
 
-    o_e = []
+    oep = []
     for m in range(M):
         o,e,p = map(int, input().split())
         o -= 1
         e -= 1
         for i in range(o, e):
             stations[i] += p
-        o_e.append((o,e,p))
+        oep.append((o,e,p))
 
-    # print("o_e")
-    # print(o_e)
+    # print("oep")
+    # print(oep)
     # print("stations")
     # print(stations)
 
     # greedyに長い数字をとっていく
-    new_o_e = []
+    new_oep = []
     for i in range(len(stations)):
         while stations[i] != 0:
             picked = pick_longest(stations, i)
-            new_o_e.append(picked)
+            new_oep.append(picked)
             # print("stations in change")
             # print(stations)
 
-    # print("new o_e")
-    # print(new_o_e)
+    # print("new oep")
+    # print(new_oep)
 
-    before = calc_whole_score(o_e, N)
-    after = calc_whole_score(new_o_e, N)
+    before = calc_whole_score(oep, N)
+    after = calc_whole_score(new_oep, N)
     return before - after
 
 
