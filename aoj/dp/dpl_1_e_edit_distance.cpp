c031a2089afe1eafca8b2090d9048fdff96c0645
minus9d <minus9d@gmail.com>
2015-07-20 12:10:13 +0900

    aoj dpl_1 解答途中のコードを追加

---
+++ aoj/dp/dpl_1_e_edit_distance.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <sstream>
+#include <string>
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <set>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <functional>
+#include <numeric>
+#include <climits>
+#include <cassert>
+
+using namespace std;
+typedef unsigned int uint;
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define REP(i,n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i,a,b) for(int i = (a); i < (int)(b); ++i)
+#define ALL(c) (c).begin(), (c).end()
+#define SIZE(v) ((int)v.size())
+
+#define pb push_back
+#define mp make_pair
+
+// 参考：https://ja.wikipedia.org/wiki/%E3%83%AC%E3%83%BC%E3%83%99%E3%83%B3%E3%82%B7%E3%83%A5%E3%82%BF%E3%82%A4%E3%83%B3%E8%B7%9D%E9%9B%A2
+int main(){
+    string s1, s2;
+    cin >> s1 >> s2;
+
+    return 0;
+}
