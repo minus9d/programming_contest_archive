d66ff28e47de55c75f44ab09e5988576bd99d577
minus9d <minus9d@gmail.com>
2016-02-29 07:38:01 +0900

    次のCPに一定速度で移動 622/1510

--- codingame/201602_codersStrikeBack/codersStrikeBack.cpp
+++ codingame/201602_codersStrikeBack/codersStrikeBack.cpp
@@ -4,6 +4,33 @@
 #include <algorithm>
 
 using namespace std;
+typedef unsigned int uint;
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define REP(i,n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i,a,b) for(int i = (a); i < (int)(b); ++i)
+#define ALL(c) (c).begin(), (c).end()
+#define SIZE(v) ((int)v.size())
+
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+
+struct P {
+    int x;
+    int y;
+};
+
+struct Pod {
+    int x;
+    int y;
+    int vx;
+    int vy;
+    int angle;
+    int nextCheckPointId;
+};
+
 
 /**
  * Auto-generated code below aims at helping you parse
@@ -13,39 +40,38 @@ int main()
 {
     int laps;
     cin >> laps; cin.ignore();
-    int checkpointCount;
-    cin >> checkpointCount; cin.ignore();
-    for (int i = 0; i < checkpointCount; i++) {
-        int checkpointX;
-        int checkpointY;
-        cin >> checkpointX >> checkpointY; cin.ignore();
+
+    int cp_num;
+    cin >> cp_num; cin.ignore();
+
+    vector<P> cps(cp_num);
+    REP(i, cp_num) {
+        cin >> cps[i].x >> cps[i].y;
+        cin.ignore();
     }
 
     // game loop
     while (1) {
-        for (int i = 0; i < 2; i++) {
-            int x;
-            int y;
-            int vx;
-            int vy;
-            int angle;
-            int nextCheckPointId;
-            cin >> x >> y >> vx >> vy >> angle >> nextCheckPointId; cin.ignore();
+        vector<Pod> pods(2);
+        REP(i,2) {
+            cin >> pods[i].x >> pods[i].y
+                >> pods[i].vx >> pods[i].vy
+                >> pods[i].angle >> pods[i].nextCheckPointId;
+            cin.ignore();
         }
-        for (int i = 0; i < 2; i++) {
-            int x;
-            int y;
-            int vx;
-            int vy;
-            int angle;
-            int nextCheckPointId;
-            cin >> x >> y >> vx >> vy >> angle >> nextCheckPointId; cin.ignore();
+        vector<Pod> opps(2);
+        REP(i,2) {
+            cin >> opps[i].x >> opps[i].y
+                >> opps[i].vx >> opps[i].vy
+                >> opps[i].angle >> opps[i].nextCheckPointId;
+            cin.ignore();
         }
 
-        // Write an action using cout. DON'T FORGET THE "<< endl"
-        // To debug: cerr << "Debug messages..." << endl;
-
-        cout << "8000 4500 100" << endl;
-        cout << "8000 4500 100" << endl;
+        cout << cps[pods[0].nextCheckPointId].x << " "
+             << cps[pods[0].nextCheckPointId].y << " "
+             << "100" << endl;
+        cout << cps[pods[1].nextCheckPointId].x << " "
+             << cps[pods[1].nextCheckPointId].y << " "
+             << "100" << endl;
     }
 }
