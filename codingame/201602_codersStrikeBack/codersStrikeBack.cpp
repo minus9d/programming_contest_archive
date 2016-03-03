84eb8ae670ea2539237cf0443ea3f35075a53220
minus9d <minus9d@gmail.com>
2016-03-03 22:36:05 +0900

    CPまでの距離に応じて速度を変える

--- codingame/201602_codersStrikeBack/codersStrikeBack.cpp
+++ codingame/201602_codersStrikeBack/codersStrikeBack.cpp
@@ -23,19 +23,40 @@ struct P {
 };
 
 struct Pod {
-    int x;
-    int y;
+    P pos;
     int vx;
     int vy;
     int angle;
     int nextCheckPointId;
 };
 
+void debug_print(Pod& p) {
+}
+
 
 /**
  * Auto-generated code below aims at helping you parse
  * the standard input according to the problem statement.
  **/
+
+vector<P> cps;
+
+int pow2(int x) { return x*x; }
+
+int dist(const P& p1, const P& p2) {
+    return sqrt(pow2(p1.x - p2.x) + pow2(p1.y - p2.y));   
+}
+
+int decide_speed(const Pod& p)
+{
+    const auto& n_cp = cps[p.nextCheckPointId];
+    // const auto& nn_cp = cps[(p.nextCheckPointId + 1) % cps.size()];
+
+    auto d = dist(p.pos, n_cp);
+    if (d > 2000) return 200;
+    else return 80;
+}
+
 int main()
 {
     int laps;
@@ -44,34 +65,44 @@ int main()
     int cp_num;
     cin >> cp_num; cin.ignore();
 
-    vector<P> cps(cp_num);
+    cps.resize(cp_num);
     REP(i, cp_num) {
         cin >> cps[i].x >> cps[i].y;
         cin.ignore();
     }
-
+    
     // game loop
+    int cnt = 0;
     while (1) {
         vector<Pod> pods(2);
         REP(i,2) {
-            cin >> pods[i].x >> pods[i].y
+            cin >> pods[i].pos.x >> pods[i].pos.y
                 >> pods[i].vx >> pods[i].vy
                 >> pods[i].angle >> pods[i].nextCheckPointId;
             cin.ignore();
         }
         vector<Pod> opps(2);
         REP(i,2) {
-            cin >> opps[i].x >> opps[i].y
+            cin >> opps[i].pos.x >> opps[i].pos.y
                 >> opps[i].vx >> opps[i].vy
                 >> opps[i].angle >> opps[i].nextCheckPointId;
             cin.ignore();
         }
 
+        debug_print(pods[0]);
+        debug_print(pods[1]);
+
+        auto speed0 = decide_speed(pods[0]);
+
         cout << cps[pods[0].nextCheckPointId].x << " "
              << cps[pods[0].nextCheckPointId].y << " "
-             << "90" << endl;
+             << speed0 << endl;
         cout << cps[pods[1].nextCheckPointId].x << " "
              << cps[pods[1].nextCheckPointId].y << " "
              << "110" << endl;
+             
+        fprintf(stderr, "\n"); 
+        ++cnt;
     }
 }
+
