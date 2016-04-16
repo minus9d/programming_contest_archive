0b3eddfe9b27099c6789470f90ed3dd7aae8cd41
minus9d <minus9d@gmail.com>
2016-04-16 23:02:27 +0900

    arc 051 コード整理

--- arc/051/c/c.py
+++ arc/051/c/c.py
@@ -1,53 +1,35 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from collections import defaultdict
+# コンテスト終了後にコード整理
 
 MOD = 1000000007
 
 N,A,B = [int(x) for x in input().split()]
-As = [int(x) for x in input().split()]
-
-arr = [(x,x) for x in As]
+arr = [int(x) for x in input().split()]
 arr.sort()
-
 
 turn = 0
 prev = None
 
-dd = defaultdict(tuple)
-
 cache = {}
 
 while True:
-    idx_order = tuple(x[1] for x in arr)
-    #print(hash(idx_order))
-    # if idx_order in dd:
-    #     print("seen!")
-    #     if dd[idx_order] == turn - 1:
-    #         pass
-    #     else:
-    # dd[idx_order] = turn
-
     # process
-    ini = arr[0]
-    new_arr = [(ini[0]*A, ini[1])] + arr[1:]
+    new_arr = [arr[0] * A] + arr[1:]
     new_arr.sort()
-
     arr = new_arr
-
-    # print(arr)
 
     turn += 1
     if turn == B:
-        print("\n".join( [str(x[0]%MOD) for x in arr] ))
+        print("\n".join( [str(x%MOD) for x in arr] ))
         break
 
+    # 十分ループした後は定常状態に入っているはず
+    # 十分ループした後、N回ループすると、順番は同じですべての要素がA倍される
     enough = 1000
     if enough <= turn < enough + N:
         cache[turn] = arr
-        idx_order = tuple(x[1] for x in arr)
-        #print("turn, idxes:", turn, idx_order)
 
     elif turn >= enough + N:
         turn = B
@@ -56,7 +38,7 @@ while True:
         v1 = diff % N
         v2 = diff // N
 
-        tmp = [(x[0] * pow(A,v2,MOD)) % MOD for x in cache[enough + v1]]
+        tmp = [(x * pow(A,v2,MOD)) % MOD for x in cache[enough + v1]]
 
         print("\n".join(str(x) for x in tmp))
         break
