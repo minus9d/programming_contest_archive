2b284020c3dfeeb9a97c226f0a9fca19fddc40af
minus9d <minus9d@gmail.com>
2018-12-16 00:43:24 +0900

    agc 029を追加

---
+++ agc/029/a/a.cpp
@@ -0,0 +1,55 @@
+#include <algorithm>
+#include <cassert>
+#include <climits>
+#include <cmath>
+#include <cstdio>
+#include <cstring>
+#include <deque>
+#include <functional>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+typedef unsigned int uint;
+typedef long long ll;
+typedef unsigned long long ull;
+
+#define REP(i,n) for(int i = 0; i < (int)(n); ++i)
+#define FOR(i,a,b) for(int i = (a); i < (int)(b); ++i)
+#define ALL(c) (c).begin(), (c).end()
+#define SIZE(v) ((int)v.size())
+
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+
+int main(void)
+{
+    string S;
+    cin >> S;
+    int len = SIZE(S);
+
+    ll b = 0;
+    ll w = 0;
+    ll ans = 0;
+    for(auto ch: S) {
+        if (ch == 'B') {
+            ++b;
+        }
+        else {
+            ++w;
+            ans += b;
+        }
+    }
+    cout << ans << endl;
+    return 0;
+}
