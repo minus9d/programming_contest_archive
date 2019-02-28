573b8c9feeb211ca2a26ffc1659f32adebd9c84a
minus9d <minus9d@gmail.com>
2019-02-28 21:49:18 +0900

    arc 092 dを追加

---
+++ arc/092/d/make_large_input.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import array
+from bisect import *
+from collections import *
+import fractions
+import heapq
+from itertools import *
+import math
+import random
+import re
+import string
+import sys
+
+N = 200000
+print(N)
+for i in range(N):
+    print(random.randint(0, 2 ** 28), end=" ")
+print()
+for i in range(N):
+    print(random.randint(0, 2 ** 28), end=" ")
+print()
+
