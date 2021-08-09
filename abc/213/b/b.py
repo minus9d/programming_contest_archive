325a8da405fb378c010c13936da226e4af646693
minus9d <minus9d@gmail.com>
2021-08-09 10:16:50 +0900

    abc 213を追加

---
+++ abc/213/b/b.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import array
+from bisect import *
+from collections import *
+import fractions
+import heapq
+from itertools import *
+import math
+import random
+import re
+import string
+import sys
+
+
+sys.setrecursionlimit(10 ** 9)
+
+
+N = int(input())
+As = list(map(int, input().split()))
+Bs = As[:]
+
+As.sort()
+tmp = As[-2]
+print(Bs.index(tmp) + 1)
+
+
+
